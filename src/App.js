30bf4f7e03b3b8b16fbbdd08e65d7c5e77f4e760
Parth Shandilya <parthsandilya20@gmail.com>
2025-04-25 02:41:13 +0530

    Hover left

--- src/App.js
+++ src/App.js
@@ -1,10 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
+import Modal from './Modal';
+import { useState } from 'react';
+
 
 function App() {
+
+
+  const [showModal, setshowModal] = useState(false)
+ 
+ function viewModal() {
+  return setshowModal(true); 
+ }
+
+ function closeModal() {
+  return setshowModal(false); 
+ }
+
+
   return (
-    <div className="App">
-     
+    <div className="App flex">
+      <div className= "h-screen w-1/6 bg-slate-900 flex-col "> 
+      <img className=" w-3/4 m-4" src={require('./images/logo.webp')} ></img>
+   <button className="bg-slate-500 px-16 rounded-lg py-2 mx-4 m-1">Dashboard</button>
+   <button className="bg-slate-500 px-16 rounded-lg py-2 mx-4 m-1">Dashboard</button>
+   <button className="bg-slate-500 px-16 rounded-lg py-2 mx-4 m-1">Dashboard</button>
+   <button className="bg-slate-500 px-16 rounded-lg py-2 mx-4 m-1">Dashboard</button>
+  
+  
+   
+       </div>
+      <div className= "h-screen w-5/6 bg-slate-700 text-center "> 
+      <h1 className="text-white text-2xl "> Click here to view details</h1>
+      <button className="bg-green-500 px-3 py-2 rounded-lg" onClick={viewModal}>View Details</button>
+      {showModal && <Modal closeModal={closeModal} />}
+      </div>
     </div>
   );
 }
---
+++ src/Modal.js
@@ -0,0 +1,29 @@
+
+
+import ViewModal from './ViewModal';
+
+function Modal ({closeModal}) {
+
+    return (   
+       <div>
+        <ViewModal/>
+      
+        <div className="w-4/5 h-4/5 bg-slate-300 rounded-2xl relative left-20 top-4 drop-shadow-lg shadow-slate-900"> 
+        
+        <div className=" flex justify-between w-full bg-slate-900 rounded-t-xl my-4 "> 
+        
+        <h1 className="text-3xl p-4 text-slate-200 relative top-2 left-4 font-thin">Hover over the image to zoom</h1>
+        <button className=" bg-red-400 p-4 m-4 h-1/20 rounded-2xl " onClick={ closeModal } >X</button>
+      
+        </div>
+        <div className="w-full relative bottom-4  "> 
+            <img src={require('./images/car.jpg')}></img>
+        </div>
+        
+        </div>
+        </div>
+        
+    )
+}
+
+export default Modal;
